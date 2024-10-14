e1d1639ead582635dbc5ca945b8d3a3dc0d9a5e0
BushraAli834 <muhammadali132078@gmail.com>
2024-10-14 19:13:26 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Card from "./components/card";
+import React from "react"
+export default function Home ()  {
+  return (
+    <>
+    <Card name='Bushra' rollno={1362} day='Monday' /><br/>
+    <Card name='Ayesha' rollno={4562} day='Tuesday' /><br/>
+    <Card name='Muskan' rollno={1098} day='Friday' /><br/>
+     </>
+  )}
